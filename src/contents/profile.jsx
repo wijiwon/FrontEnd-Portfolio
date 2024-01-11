384d2c0302fa1db573ab770e157e856e98045933
wijiwon <dnlwldnjs5@gmail.com>
2024-01-11 15:02:10 +0900

    기술스택 수정

--- src/contents/profile.jsx
+++ src/contents/profile.jsx
@@ -1,5 +1,5 @@
 import { Hero } from '../components/common'
-import { IoLogoGithub } from 'react-icons/io'
+import { IoIosCall, IoLogoGithub } from 'react-icons/io'
 import { TfiEmail } from 'react-icons/tfi'
 import { FaBlogger } from 'react-icons/fa6'
 import { useState, useRef, useEffect } from 'react'
@@ -54,6 +54,10 @@ const Profile = () => {
                 </Hero>
                 <p className="text-slate-700 pt-[5px] md:text-[24px] md:pt-[10px] text-[16px]">Front-End 개발자</p>
                 <p className="flex flex-col text-[16px] items-left pt-[7px] md:pt-[5px] md:text-[14px]">
+                    <p className="flex items-center gap-[5px] text-slate-600">
+                        <IoIosCall />
+                        010-3370-3224
+                    </p>
                     <a href="mailto:dnlwldnjs5@gmail.com" className="flex items-center gap-[5px] text-slate-600">
                         <TfiEmail />
                         dnlwldnjs5@gmail.com
--- src/contents/skills.jsx
+++ src/contents/skills.jsx
@@ -1,6 +1,16 @@
 import { Wrapper, Heading } from '../components/common'
-import { DiJavascript, DiNodejs, DiGithubBadge, DiMysql } from 'react-icons/di'
-import { SiNextdotjs, SiTypescript, SiJavascript, SiExpress, SiReact, SiSolidity } from 'react-icons/si'
+import { DiJavascript, DiNodejs, DiGithubBadge, DiMysql, DiPhotoshop } from 'react-icons/di'
+import { FaAws, FaFigma, FaHtml5, FaNodeJs } from 'react-icons/fa6'
+import {
+    SiNextdotjs,
+    SiTypescript,
+    SiJavascript,
+    SiExpress,
+    SiReact,
+    SiSolidity,
+    SiTailwindcss,
+    SiStyledcomponents,
+} from 'react-icons/si'
 
 const Skills = () => {
     return (
@@ -18,8 +28,12 @@ const Skills = () => {
                                 <span className="md:w-[120px]">Javascript</span>
                             </span>
                             <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
-                                <li>배열과 문자열의 메소드 활용 가능</li>
-                                <li>구조 분해 할당 사용</li>
+                                <li>자바스크립트 실행 코드 환경을 설정하는 실행 컨텍스트에 대해 학습하였습니다.</li>
+                                <li>
+                                    서버와의 통신을 위한 비동기 처리를 이해하고 async/await을 사용해 비동기 작업을
+                                    관리하였습니다.
+                                </li>
+                                <li>이벤트 루프에 대해 이해하여 비동기 코드를 처리하였습니다.</li>
                             </ul>
                         </li>
 
@@ -29,8 +43,9 @@ const Skills = () => {
                                 <span className="md:w-[120px]">Typescript</span>
                             </span>
                             <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
-                                <li>Typescript 환경설정 가능</li>
-                                <li>interface와 type의 차이를 암</li>
+                                <li>Typescript의 환경 설정이 가능합니다.</li>
+                                <li>코드의 안정성을 위해 interface로 객체의 구조를 정의하였습니다.</li>
+                                <li>컴포넌트에 제네릭타입을 활용하여 재사용성을 높였습니다.</li>
                             </ul>
                         </li>
                         <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
@@ -39,8 +54,11 @@ const Skills = () => {
                                 <span className="md:w-[120px]">Solidity</span>
                             </span>
                             <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
-                                <li>ERC20 개념 숙지</li>
-                                <li>Dex 구조 숙지</li>
+                                <li>
+                                    ERC20을 기반으로 한 스마트계약 코드를 구현하였으며 remix를 이용한 트랙잭션을
+                                    배포하였습니다.
+                                </li>
+                                <li>Dex거래소의 구조를 숙지하였습니다.</li>
                             </ul>
                         </li>
                     </ul>
@@ -57,8 +75,9 @@ const Skills = () => {
                                 <span className="md:w-[120px]">React</span>
                             </span>
                             <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
-                                <li>재사용성을 고려하여 컴포넌트 구성하려고 노력</li>
-                                <li>기능 구현 시 리렌더링을 최소화하고자 노력</li>
+                                <li>Lifecycle을 학습하여 React 동작 원리에 대해 이해하였습니다.</li>
+                                <li>재사용성을 고려하여 컴포넌트 구성하려고 노력하고 있습니다.</li>
+                                <li>redux를 이용한 간단한 전역 상태관리 경험이 있습니다.</li>
                             </ul>
                         </li>
                     </ul>
@@ -71,11 +90,12 @@ const Skills = () => {
                     <ul className="flex flex-col gap-[16px] text-[16px] pb-[32px] font-medium w-full items-start">
                         <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
                             <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
-                                <SiExpress />
-                                <span className="md:w-[120px]">Express</span>
+                                <FaNodeJs />
+                                <span className="md:w-[120px]">Node.js</span>
                             </span>
                             <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
-                                <li>RestFul API 스럽게 할라고 노력함!</li>
+                                <li>Sequelize를 이용하여 데이터베이스를 관리하였습니다.</li>
+                                <li>Express를 이용한 웹서버 통신이 가능합니다.</li>
                             </ul>
                         </li>
                     </ul>
@@ -93,6 +113,24 @@ const Skills = () => {
                             <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
                                 <li>Workbench 사용하여 DB 관리 가능</li>
                             </ul>
+                        </li>
+                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline mt-4">
+                            <span className="text-[16px] text-slate-600 md:text-[20px] basis-[20%] flex items-center gap-[8px]">
+                                <SiTailwindcss />
+                                <span className="md:w-[100px]">Tailwind</span>
+                            </span>
+                            <span className="text-[16px] text-slate-600 md:text-[20px] basis-[20%] flex items-center gap-[8px]">
+                                <FaAws />
+                                <span className="md:w-[100px]">AWS</span>
+                            </span>
+                            <span className="text-[16px] text-slate-600 md:text-[20px] basis-[20%] flex items-center gap-[8px]">
+                                <DiPhotoshop />
+                                <span className="md:w-[100px]">Photoshop</span>
+                            </span>
+                            <span className="text-[16px] text-slate-600 md:text-[20px] basis-[20%] flex items-center gap-[8px]">
+                                <FaFigma />
+                                <span className="md:w-[100px]">Figma</span>
+                            </span>
                         </li>
                     </ul>
                 </li>
